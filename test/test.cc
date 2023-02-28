211eeddb91906eba3a1cb60c1004b27c507d8932
zhangyu-hesaitech <zhangyu@hesaitech.com>
2023-02-28 19:04:16 +0800

    V1.2.46 update default correction path

--- test/test.cc
+++ test/test.cc
@@ -48,7 +48,7 @@ void rawcallback(PandarPacketsArray *array) {
 int main(int argc, char** argv) {
     boost::shared_ptr<PandarSwiftSDK> spPandarSwiftSDK;
     spPandarSwiftSDK.reset(new PandarSwiftSDK(std::string("192.168.1.201"), "", 2368, 10110, std::string("PandarFT"), \
-                                std::string("../params/PandarFT_Correction.csv"), \
+                                std::string("../params/PandarFT_Correction.dat"), \
                                 std::string(""), \
                                 std::string(""), lidarCallback, rawcallback, gpsCallback, \
                                 std::string(""), \
